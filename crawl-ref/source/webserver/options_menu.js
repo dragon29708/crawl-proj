2af5693453b9f334f5b11cd82341c8bbc45494b3
Andrew Cassidy <50083911+dragon29708@users.noreply.github.com>
2021-04-12 19:52:02 -0400

    will's js for options_menu

---
+++ crawl-ref/source/webserver/options_menu.js
@@ -0,0 +1,12 @@
+
+function optionClicked(val) {
+var menus = {
+    "0":"Commands",
+    "1":"Inscriptions",
+    "2":"Interactions",
+    "3":"Travel",
+    "4":"Messages",
+    "5":"Visual"
+}
+alert(menus[val] + " clicked");
+}
