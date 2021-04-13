4946ea93251132e11e55fde2a7f080af4cd8ebfb
Andrew Cassidy <50083911+dragon29708@users.noreply.github.com>
2021-04-13 14:42:28 -0400

    Create options-menu.cc

---
+++ crawl-ref/source/options-menu.cc
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * @brief Functions related to the options menu.
+**/
+
+#include "options-menu.h"
+
+// calls the options menu
+void call_gui() {
+
+  // Will do this
+  
+  return;
+  
+} // call_gui()
+
+
+// read from options text file
+bool read_options() {
+
+  return;
+  
+} // read_options()
+
+// write to options text file
+bool write_options() {
+
+  return;
+  
+} // write_options()
