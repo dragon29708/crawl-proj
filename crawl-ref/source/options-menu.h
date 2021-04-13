de2a9d4e53d7f82e454e1f8ba36e28e440fd9c4b
Andrew Cassidy <50083911+dragon29708@users.noreply.github.com>
2021-04-13 14:35:07 -0400

    Update options-menu.h

--- crawl-ref/source/options-menu.h
+++ crawl-ref/source/options-menu.h
@@ -4,3 +4,27 @@
 **/
 
 #pragma once
+
+// calls the options menu
+void call_gui() {
+
+  // Will do this
+  
+  return
+  
+} // call_gui()
+
+
+// read from options text file
+bool read_options() {
+
+  return;
+  
+} // read_options()
+
+// write to options text file
+bool write_options() {
+
+  return;
+  
+} // write_options()
